4d6a0215b979ac790c8b4cad9edcfacafb5df84b
Ian Su <sui1@uci.edu>
2026-06-09 13:46:46 -0700

    Added update_event.py

--- update_event.py
+++ update_event.py
@@ -1,3 +1,29 @@
+from db import get_connection
+
+
 def update_event(args):
     # args: [eid, title, datetime]
-    raise NotImplementedError
+    eid = int(args[0])
+    title = args[1]
+    event_datetime = args[2]
+
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE Event SET title = %s, datetime = %s WHERE eid = %s",
+            (title, event_datetime, eid),
+        )
+        if cur.rowcount > 0:
+            conn.commit()
+            return True
+        conn.rollback()
+        return False
+    except Exception:
+        if conn:
+            conn.rollback()
+        return False
+    finally:
+        if conn:
+            conn.close()
