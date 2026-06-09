249acff607f9a60682a29dcab5e3f1f342357c4a
KobeyThompson <kobeythompson@gmail.com>
2026-06-09 00:25:36 -0700

    working delete_organizer.py and venue_events.py

--- delete_organizer.py
+++ delete_organizer.py
@@ -1,3 +1,25 @@
+from db import get_connection
+
+
 def delete_organizer(args):
-    # args: [uid]
-    raise NotImplementedError
+    uid = int(args[0])
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(
+            "DELETE FROM Organizer WHERE uid = %s",
+            (uid,)
+        )
+
+        connection.commit()
+        return cursor.rowcount > 0
+
+    except Exception:
+        connection.rollback()
+        return False
+
+    finally:
+        cursor.close()
+        connection.close()
--- venue_events.py
+++ venue_events.py
@@ -1,3 +1,26 @@
+from db import get_connection
+
+
 def venue_events(args):
-    # args: [vid]
-    raise NotImplementedError
+    vid = int(args[0])
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT Event.eid, Event.title, Event.type, Event.datetime, Hosting.is_primary
+        FROM Event
+        JOIN Hosting ON Event.eid = Hosting.eid
+        WHERE Hosting.vid = %s
+        ORDER BY Event.datetime ASC, Event.eid ASC
+        """,
+        (vid,)
+    )
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return rows
