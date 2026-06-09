1e36fc4812fc661b12b1a705faba125f3d2c719c
Ian Su <sui1@uci.edu>
2026-06-09 03:07:10 -0700

    Added functionality for insert admin and participant schedule.

--- insert_admin.py
+++ insert_admin.py
@@ -1,3 +1,33 @@
+from db import get_connection
+
+
 def insert_admin(args):
     # args: [uid, email, username, joined, firstname, lastname]
-    raise NotImplementedError
+    uid = int(args[0])
+    email = args[1]
+    username = args[2]
+    joined = args[3]
+    firstname = args[4]
+    lastname = args[5]
+
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO User (uid, email, username, joined) VALUES (%s, %s, %s, %s)",
+            (uid, email, username, joined),
+        )
+        cur.execute(
+            "INSERT INTO Administrator (uid, firstname, lastname) VALUES (%s, %s, %s)",
+            (uid, firstname, lastname),
+        )
+        conn.commit()
+        return True
+    except Exception:
+        if conn:
+            conn.rollback()
+        return False
+    finally:
+        if conn:
+            conn.close()
--- participant_schedule.py
+++ participant_schedule.py
@@ -1,3 +1,27 @@
+from db import get_connection
+
+
 def participant_schedule(args):
-    # args: [uid]
-    raise NotImplementedError
+    uid = int(args[0])
+
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT e.eid, e.title, e.type, e.datetime, s.snum,
+                   v.vid, v.street, v.city, v.state, v.zip
+            FROM Slot s
+            JOIN Event e ON s.eid = e.eid
+            LEFT JOIN Hosting h ON e.eid = h.eid AND h.is_primary = 1
+            LEFT JOIN Venue v ON h.vid = v.vid
+            WHERE s.uid = %s AND s.is_reserved = 1
+            ORDER BY e.datetime ASC, e.eid ASC
+            """,
+            (uid,),
+        )
+        return cur.fetchall()
+    finally:
+        if conn:
+            conn.close()
